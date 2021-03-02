854e541d09fa89a01429932b07e9d13eb452775e
blackzshaik <blackzshaik@gmail.com>
2021-03-02 20:33:07 +0530

    added pager indicator

--- app/src/main/java/com/madtutorials/viewpager2example/MainActivity.kt
+++ app/src/main/java/com/madtutorials/viewpager2example/MainActivity.kt
@@ -3,13 +3,21 @@ package com.madtutorials.viewpager2example
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.madtutorials.viewpager2example.adapter.ImageViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<ViewPager2>(R.id.viewPager).adapter = ImageViewPagerAdapter()
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+            viewPager.adapter = ImageViewPagerAdapter()
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        TabLayoutMediator(tabLayout,viewPager){tab, position ->
+
+
+        }.attach()
 
     }
 }
